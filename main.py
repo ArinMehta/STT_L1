3423cb92240d2f0da309e7017b95223dbdd01e31
ArinMehta <167913165+ArinMehta@users.noreply.github.com>
2025-09-05 13:56:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+x=10;
+y=20
+
+for i in range(x,y):
+  print("Count:", i);
+
+def Multiply(a,b):
+  result=a*B
+  return result
+
+def Add(a,b):
+  result=a+b
+  return result
+
+answer = multiply(a,b)
+print("Answer is:", answer)
+
+answer2 = add(a,b)
+print("Addition is: ", answer2)
+
+if answer==answer2:
+  print("True)
+
+else:
+  print("False")
+
+def swap(a,b):
+  return b,a
+
+first = 1
+second =2
+print("Before Swapping:", first, second)
+swap(first, second)
+print("After Swapping:", first, second)
+
+''' Is this a comment?"""
