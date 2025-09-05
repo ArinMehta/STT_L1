52f1e6e577d03d87755a63b13e6355c09236e597
ArinMehta <167913165+ArinMehta@users.noreply.github.com>
2025-09-05 15:16:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-x=10;
+"""Main module for practicing pylint fixes."""
+
+x=10
 y=20
 
 for i in range(x,y):
-  print("Count:", i);
+    print("Count:", i)
 
-def Multiply(a,b):
-  result=a*B
-  return result
+def multiply(a,b):
+    """Multiply two numbers."""
+    result=a*b
+    return result
 
-def Add(a,b):
-  result=a+b
-  return result
+def add(a,b):
+    """ Add two numbers."""
+    result=a+b
+    return result
 
-answer = multiply(a,b)
+answer = multiply(x,y)
 print("Answer is:", answer)
 
-answer2 = add(a,b)
+answer2 = add(x,y)
 print("Addition is: ", answer2)
 
 if answer==answer2:
-  print("True")
+    print("True")
 
 else:
-  print("False")
+    print("False")
 
 def swap(a,b):
-  return b,a
+    """ Swap two numbers."""
+    return b,a
 
 first = 1
 second =2
 print("Before Swapping:", first, second)
-swap(first, second)
+first, second = swap(first, second)
 print("After Swapping:", first, second)
 
-''' 
-Is this a comment?
-'''
+'''  Is this a comment? '''
